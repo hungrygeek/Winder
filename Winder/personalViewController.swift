840e2cca73170e7990fd0ccaa60ec9e4c39bcb26
Shi Shu <s.shu@wustl.edu>
2016-11-12 18:07:07 -0600

    new personal view controller file.

---
+++ Winder/personalViewController.swift
@@ -0,0 +1,24 @@
+//
+//  personalViewController.swift
+//  Winder
+//
+//  Created by ShiShu on 11/12/16.
+//  Copyright © 2016 cse438. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonalViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
