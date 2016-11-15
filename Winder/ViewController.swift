007357fd874f5492c52bb64bae173846cb303f65
Rokee <rokee.lv@gmail.com>
2016-11-15 11:15:36 -0600

    no message

--- Winder/ViewController.swift
+++ Winder/ViewController.swift
@@ -13,13 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let vc = MatchViewController()
+//        vc.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+//        presentViewController(vc, animated: false, completion: nil)
+//        print("go")
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        let vc = MatchViewController()
+        vc.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        presentViewController(vc, animated: false, completion: nil)
+        print("go")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
-
-
+    
+    
 }
 
