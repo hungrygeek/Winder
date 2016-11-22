f6fa8cc2e6b145d38cfb3e1a942364a4089507bb
Rokee <rokee.lv@gmail.com>
2016-11-22 16:48:40 -0600

    fix ship bar

---
+++ Winder/CustomOverlayView.swift
@@ -0,0 +1,39 @@
+//
+//  OverlayView.swift
+//  Winder
+//
+//  Created by Rokee on 11/20/16.
+//  Copyright © 2016 cse438. All rights reserved.
+//
+
+
+import UIKit
+import Koloda
+
+private let overlayRightImageName = "yesOverlayImage"
+private let overlayLeftImageName = "noOverlayImage"
+
+class CustomOverlayView: OverlayView {
+    
+    @IBOutlet lazy var overlayImageView: UIImageView!={
+        [unowned self] in
+        
+        var imageView = UIImageView(frame: self.bounds)
+        self.addSubview(imageView)
+        return imageView
+        
+    }()
+    
+    override var overlayState: SwipeResultDirection?{
+        didSet{
+            switch overlayState {
+            case .Left?:
+                overlayImageView.image = UIImage(named: overlayLeftImageName)
+            case .Right?:
+                overlayImageView.image = UIImage(named: overlayRightImageName)
+            default:
+                overlayImageView.image = nil
+            }
+        }
+    }
+}
--- Winder/MatchViewController.swift
+++ Winder/MatchViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import Koloda
+
+private var numberOfCards: UInt = 5
 
 class MatchViewController:UIViewController{
     
 //    var profileButton
+//    @IBOutlet weak var kv: KolodaView!
     
+    var kolodaView2: KolodaView = {
+        var kv: KolodaView = KolodaView(frame: CGRect(x:0,y: 0,width:100,height:100))
+        return kv
+    }()
+    
+    private var dataSource: Array<UIImage> = {
+        var array: Array<UIImage> = []
+        for index in 0..<numberOfCards {
+            array.append(UIImage(named: "round_\(index + 1)")!)
+        }
+        
+        return array
+    }()
+
     var nameLabel:UILabel = {
         let label = UILabel()
         label.text = "Cat Wang"
@@ -63,6 +81,11 @@ class MatchViewController:UIViewController{
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+//        kolodaView.dataSource = self
+//        kolodaView.delegate = self
+        
+        kolodaView2.dataSource = self
+        kolodaView2.delegate = self
         
         likeButton.center = CGPoint(x: view.frame.width-likeButton.frame.width, y: view.frame.height-likeButton.frame.height)
         view.addSubview(likeButton)
@@ -80,7 +103,45 @@ class MatchViewController:UIViewController{
         schoolLabel.center = CGPoint(x: view.frame.midX, y: view.frame.height-likeButton.frame.height*1.618)
         view.addSubview(schoolLabel)
         
+        kolodaView2.center = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        view.addSubview(kolodaView2)
         view.backgroundColor = UIColor.whiteColor()
     }
     
+}
+
+//MARK: KolodaViewDelegate
+extension MatchViewController: KolodaViewDelegate {
+    
+    func kolodaDidRunOutOfCards(koloda: KolodaView) {
+        print("no cards more")
+//        dataSource.insert(UIImage(named: "Card_like_6")!, atIndex: kolodaView2.currentCardIndex - 1)
+//        let position = kolodaView2.currentCardIndex
+//        kolodaView2.insertCardAtIndexRange(position...position, animated: true)
+    }
+    
+    func koloda(koloda: KolodaView, didSelectCardAtIndex index: UInt) {
+//        UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
+        print("you click")
+    }
+}
+
+//MARK: KolodaViewDataSource
+extension MatchViewController: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(koloda:KolodaView) -> UInt {
+        return UInt(dataSource.count)
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardAtIndex index: UInt) -> UIView {
+        return UIImageView(image: dataSource[Int(index)])
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
+
+        let ov = NSBundle.mainBundle().loadNibNamed("OverlayView",
+                                                   owner: self, options: nil)[0] as? OverlayView
+        print(OverlayView())
+        return ov
+    }
 }
