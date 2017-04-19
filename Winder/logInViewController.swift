5ec49fadc36d69e11938f9d0d44949e63c38f2b8
Rokee <rokee.lv@gmail.com>
2017-04-19 15:35:34 -0500

    no message

--- Winder/logInViewController.swift
+++ Winder/logInViewController.swift
@@ -1,49 +1,35 @@
 //
-//  logInViewController.swift
+//  LoginViewController.swift
 //  Winder
 //
-//  Created by Yugui Chen on 16/11/12.
-//  Copyright © 2016年 cse438. All rights reserved.
+//  Created by Rokee on 4/10/17.
+//  Copyright © 2017 Winder. All rights reserved.
 //
 
 import UIKit
 
-class logInViewController: UIViewController {
-    
+class LoginViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let backgroundPic = UIImageView()
-        backgroundPic.frame = self.view.frame
-        backgroundPic.center = self.view.center
-        backgroundPic.image = UIImage(named: "background")
-        backgroundPic.alpha = 0.5
-        self.view.addSubview(backgroundPic)
-        
-        let logoImage = UIImageView()
-        logoImage.frame = CGRectMake(0, 0, 300, 300)
-        logoImage.center = CGPoint(x: self.view.center.x,y: 200)
-        logoImage.image = UIImage(named: "logo")
-        self.view.addSubview(logoImage)
-        
-        let logInUserName = UITextField()
-        logInUserName.frame = CGRectMake(0, 0, 300, 50)
-        logInUserName.center = CGPoint(x: self.view.center.x, y: 400)
-        logInUserName.layer.borderColor = UIColor.blackColor().CGColor
-        logInUserName.layer.backgroundColor = UIColor.whiteColor().CGColor
-        logInUserName.alpha = 0.8
-        logInUserName.layer.cornerRadius = 10
-        self.view.addSubview(logInUserName)
-        
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-
-
